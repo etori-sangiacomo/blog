3c50901276f0c3cd9defe6e25802d7b06483e60f
Étori Sangiacomo <etori.sangiacomo@caminoeducation.com>
2021-03-07 20:42:41 -0300

    set keys

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 echo "Start Gigalixir Deploy"
-git remote add gigalixir https://$GIGALIXIR_EMAIL:$GIGALIXIR_API_KEY@git.gigalixir.com/$GIGALIXIR_APP_NAME.git
+git remote add gigalixir https://etoribadanai@gmail.com:bc392415-ae23-46b9-a374-7363867514db@git.gigalixir.com/blogetori.git
 git push gigalixir master
 echo "Finish Gigalixir Deploy"
