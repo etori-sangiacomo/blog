caab1ff1e15f55c8b32eb9728b45d1048dd5e1bd
Étori Sangiacomo <etori.sangiacomo@caminoeducation.com>
2021-03-07 22:02:46 -0300

    install gigalixir in travis

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 echo "Start Gigalixir Deploy"
+pip3 install gigalixir --user
 git remote add gigalixir https://$GIGALIXIR_EMAIL:$GIGALIXIR_API_KEY@git.gigalixir.com/$GIGALIXIR_APP_NAME.git
 git push gigalixir master
 echo "Finish Gigalixir Deploy"
