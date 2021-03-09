40f3a4f584eaf9ff47e2e2368a175442ef9dad4b
Étori Sangiacomo <etori.sangiacomo@caminoeducation.com>
2021-03-08 22:59:56 -0300

    adjusting deploy

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 echo "Start Gigalixir Deploy"
-pip3 install gigalixir --user
 git remote add gigalixir https://$GIGALIXIR_EMAIL:$GIGALIXIR_API_KEY@git.gigalixir.com/$GIGALIXIR_APP_NAME.git
-git push gigalixir master
+mix test 
+git push -f gigalixir HEAD:refs/heads/main
 echo "Finish Gigalixir Deploy"
