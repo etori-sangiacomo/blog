9545fd4537afafb5b8d673e32aa4e78fa05adb35
Étori Sangiacomo <etori.sangiacomo@caminoeducation.com>
2021-04-03 01:06:54 -0300

    comments flow, initial socket comments

--- lib/blog/posts.ex
+++ lib/blog/posts.ex
@@ -5,6 +5,10 @@ defmodule Blog.Posts do
 
   def get_post!(id), do: Repo.get!(Post, id)
 
+  def get_post_with_comments(id) do
+    Repo.get!(Post, id) |> Repo.preload(:comments)
+  end
+
   def create_post(attrs \\ %{}) do
     Post.changeset(attrs)
     |> Repo.insert()
